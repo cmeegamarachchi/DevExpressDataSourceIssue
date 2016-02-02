7c51992c3c9275df412b359e0051cea9077f96ab
Chintana Meegamarachchi <chintana@appsolve.com.au>
2016-02-03 07:47:10 +0800

    Initial commit

---
+++ DevExpressDataSource/DevExpressDataSource/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using NorthwindBusinessServices.Products;
+using NorthwindDataAccess.DataContext;
+
+namespace DevExpressDataSource
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public IEnumerable<ProductDetails> ProductDetails { get; set; }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // you need to have northwind database configured and pointed via app config
+            // for the following two lined to work
+            var service = new ProductsService(new NorthwindUnitOfWork());
+            ProductDetails = service.GetProducts(int.MaxValue, 1).Data;
+        }
+    }
+}
